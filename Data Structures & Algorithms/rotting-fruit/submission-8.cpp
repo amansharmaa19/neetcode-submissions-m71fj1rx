ebc834f48ccabb12436a767194536d2ef239950d
amansharmaa19 <amansharma19696@gmail.com>
2026-04-07 09:59:15 +0530

    Add: rotting-fruit - submission-8

---
+++ Data Structures & Algorithms/rotting-fruit/submission-8.cpp
@@ -0,0 +1,85 @@
+class Solution {
+    bool isSafe(
+        vector<vector<int>>& grid,
+        int row,
+        int col,
+        int rows,
+        int cols
+    ) {
+        if (row < 0 || row >= rows || col < 0 || col >= cols || grid[row][col] == 2 || grid[row][col] == 0) {
+            return false;
+        }
+        return true;
+    }
+
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        if (rows == 0) {
+            return -1;
+        }
+        int cols = grid[0].size();
+        bool hasFreshFruits = false;
+        queue<pair<int, int>> q;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 2) {
+                    q.push({i, j});
+                }
+                if (grid[i][j] == 1) {
+                    hasFreshFruits = true;
+                }
+            }
+        }
+
+        if (q.empty()) {
+            if (hasFreshFruits) {
+                return -1;
+            }
+            return 0;
+        }
+
+        int minutes = 0;
+        while (!q.empty()) {
+            int size = q.size();
+            bool isRotten = false;
+            while (size--) {
+                pair<int, int> front = q.front();
+                int row = front.first;
+                int col = front.second;
+                q.pop();
+                if (isSafe(grid, row + 1, col, rows, cols)) {
+                    q.push({row + 1, col});
+                    grid[row + 1][col] = 2;
+                    isRotten = true;
+                }
+                if (isSafe(grid, row - 1, col, rows, cols)) {
+                    q.push({row - 1, col});
+                    grid[row - 1][col] = 2;
+                    isRotten = true;
+                }
+                if (isSafe(grid, row, col + 1, rows, cols)) {
+                    q.push({row, col + 1});
+                    grid[row][col + 1] = 2;
+                    isRotten = true;
+                }
+                if (isSafe(grid, row, col - 1, rows, cols)) {
+                    q.push({row, col - 1});
+                    grid[row][col - 1] = 2;
+                    isRotten = true;
+                }
+            }
+            if (isRotten) {
+                minutes++;
+            }
+        }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 1) {
+                    return -1;
+                }
+            }
+        }
+        return minutes;
+    }
+};
