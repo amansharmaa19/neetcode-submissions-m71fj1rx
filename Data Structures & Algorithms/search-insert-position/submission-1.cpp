2db1efe6f91c9ccecba0864ce8c3a25909321fdd
amansharmaa19 <amansharma19696@gmail.com>
2026-04-09 22:12:12 +0530

    Add: search-insert-position - submission-1

---
+++ Data Structures & Algorithms/search-insert-position/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (target > nums[mid] && target <= nums[mid + 1]) {
+                return mid + 1;
+            } else if (target > nums[mid]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        if (target > nums[nums.size() - 1]) {
+            return nums.size();
+        }
+        return 0;
+    }
+};
