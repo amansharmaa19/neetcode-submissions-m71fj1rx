ebc231022f58318c1c6d53c13469eeb165243465
amansharmaa19 <amansharma19696@gmail.com>
2026-04-23 16:39:07 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    int climbStairsUtil(int n, vector<int>& dp) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+        return dp[n] = climbStairsUtil(n - 1, dp) + climbStairsUtil(n - 2, dp);
+    }
+
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n + 1, -1);
+        return climbStairsUtil(n, dp);
+    }
+};
