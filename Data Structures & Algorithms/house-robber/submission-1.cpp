92e6af03c16e1a22d17d3debb2f2547788b826a1
amansharmaa19 <amansharma19696@gmail.com>
2026-04-25 14:46:02 +0530

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    int solve(int index, vector<int>& nums, vector<int>& dp) {
+        if (index == 0) {
+            return nums[0];
+        } else if (index < 0) {
+            return 0;
+        }
+        if (dp[index] != -1) {
+            return dp[index];
+        }
+        int pick = nums[index] + solve(index - 2, nums, dp);
+        int notPick = 0 + solve(index - 1, nums, dp);
+        return dp[index] = max(pick, notPick);
+    }
+
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, -1);
+        return solve(n - 1, nums, dp);
+    }
+};
