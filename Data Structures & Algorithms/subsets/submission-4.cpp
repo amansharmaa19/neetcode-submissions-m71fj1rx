eda835f100c1da7c25c60779de3280b834958878
amansharmaa19 <amansharma19696@gmail.com>
2026-05-01 15:29:45 +0530

    Add: subsets - submission-4

---
+++ Data Structures & Algorithms/subsets/submission-4.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    void subsetsUtil(
+        int index,
+        vector<int>& nums,
+        vector<int>& eachSubset,
+        vector<vector<int>>& result
+    ) {
+        result.push_back(eachSubset);
+        for (int i = index; i < nums.size(); i++) {
+            eachSubset.push_back(nums[i]);
+            subsetsUtil(i + 1, nums, eachSubset, result);
+            eachSubset.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> result;
+        vector<int> eachSubset;
+        subsetsUtil(0, nums, eachSubset, result);
+        return result;
+    }
+};
