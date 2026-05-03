fecd13afc574374f7d1a1cf3484adbb6de4d28cb
amansharmaa19 <amansharma19696@gmail.com>
2026-05-03 17:18:03 +0530

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.cpp
@@ -0,0 +1,69 @@
+class PrefixTree {
+    class Node {
+        Node *nodes[26];
+        bool isLast;
+    public:
+        Node() {
+            for (int i = 0; i < 26; i++) {
+                nodes[i] = nullptr;
+            }
+            isLast = false;
+        }
+        bool containsKey(char c) {
+            if (nodes[c - 'a'] != nullptr) {
+                return true;
+            }
+            return false;
+        }
+        void putNode(char c, Node* node) {
+            nodes[c - 'a'] = node;
+        }
+        Node* getNext(char c) {
+            return nodes[c - 'a'];
+        }
+        void setLast() {
+            isLast = true;
+        }
+        bool getLast() {
+            return isLast;
+        }
+    };
+    Node *root;
+public:
+    PrefixTree() {
+        root = new Node();
+    }
+    
+    void insert(string word) {
+        Node* curr = root;
+        for (int i = 0; i < word.size(); i++) {
+            if (!curr->containsKey(word[i])) {
+                curr->putNode(word[i], new Node());
+            }
+            curr = curr->getNext(word[i]);
+        }
+        curr->setLast();
+    }
+    
+    bool search(string word) {
+        Node* curr = root;
+        for (int i = 0; i < word.size(); i++) {
+            if (!curr->containsKey(word[i])) {
+                return false;
+            }
+            curr = curr->getNext(word[i]);
+        }
+        return curr->getLast();
+    }
+    
+    bool startsWith(string prefix) {
+        Node* curr = root;
+        for (int i = 0; i < prefix.size(); i++) {
+            if (!curr->containsKey(prefix[i])) {
+                return false;
+            }
+            curr = curr->getNext(prefix[i]);
+        }
+        return true;
+    }
+};
