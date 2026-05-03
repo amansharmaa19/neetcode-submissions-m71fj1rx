705be4a22aa676528c075f25db0211b4da57c102
amansharmaa19 <amansharma19696@gmail.com>
2026-05-03 13:41:53 +0530

    Add: valid-sudoku - submission-4

---
+++ Data Structures & Algorithms/valid-sudoku/submission-4.cpp
@@ -0,0 +1,64 @@
+class Solution {
+    bool isValid(vector<vector<char>>& board, char& c, int& row, int& col) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == c) {
+                return false;
+            }
+            if (board[i][col] == c) {
+                return false;
+            }
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+public:
+    bool solveSudoku(vector<vector<char>>& board) {
+        int rows = board.size();
+        int cols = board[0].size();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (board[i][j] == '.') {
+                    // Check to place a valid number
+                    for (char c = '1'; c <= '9'; c++) {
+                        if (isValid(board, c, i, j)) {
+                            board[i][j] = c;
+                            if (solveSudoku(board)) {
+                                return true;
+                            } else {
+                                board[i][j] = '.';
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<vector<int>> rows(9, vector<int>(9, 0));
+        vector<vector<int>> cols(9, vector<int>(9, 0));
+        vector<vector<int>> boxes(9, vector<int>(9, 0));
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    continue;
+                }
+
+                int num = board[i][j] - '1';
+                int box = ((i / 3) * 3) + (j / 3);
+
+                if (rows[i][num] || cols[j][num] || boxes[box][num]) {
+                    return false;
+                }
+                rows[i][num] = cols[j][num] = boxes[box][num] = 1;
+            }
+        }
+        return true;
+    }
+};
