4721e46b38fe40c89b48e9132b21d441d98b5887
amansharmaa19 <amansharma19696@gmail.com>
2026-05-05 00:14:48 +0530

    Add: subarray-sum-equals-k - submission-0

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> hash;
+        hash[0] = 1;
+        int sum = 0, totalSubArrays = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            if (hash.find(sum - k) != hash.end()) {
+                totalSubArrays += hash[sum - k];
+            }
+            hash[sum]++;
+        }
+        return totalSubArrays;
+    }
+};
