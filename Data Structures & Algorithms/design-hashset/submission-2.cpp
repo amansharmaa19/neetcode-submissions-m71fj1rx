ce079e49fc0f6f98cff693bf7fc35c4e3f2f8881
amansharmaa19 <amansharma19696@gmail.com>
2026-05-25 00:51:14 +0530

    Add: design-hashset - submission-2

---
+++ Data Structures & Algorithms/design-hashset/submission-2.cpp
@@ -0,0 +1,28 @@
+class MyHashSet {
+    vector<bool> hashTable;
+public:
+    MyHashSet() : hashTable(1000001, false) {}
+    
+    void add(int key) {
+        hashTable[key] = true;
+    }
+    
+    void remove(int key) {
+        hashTable[key] = false;
+    }
+    
+    bool contains(int key) {
+        if (hashTable[key]) {
+            return true;
+        }
+        return false;
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
