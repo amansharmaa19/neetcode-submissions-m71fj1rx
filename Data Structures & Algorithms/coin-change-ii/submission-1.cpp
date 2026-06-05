1c15837934591b15b62716fc328a18b02dd5f661
amansharmaa19 <amansharma19696@gmail.com>
2026-06-05 15:52:50 +0530

    Add: coin-change-ii - submission-1

---
+++ Data Structures & Algorithms/coin-change-ii/submission-1.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    int coinChangeUtil(
+        int index,
+        int amount,
+        vector<int>& coins,
+        vector<vector<int>>& dp
+    ) {
+        if (index == 0) {
+            if (amount % coins[0] == 0) {
+                return 1;
+            }
+            return 0;
+        }
+        if (dp[index][amount] != -1) {
+            return dp[index][amount];
+        }
+        int notPick = coinChangeUtil(index - 1, amount, coins, dp);
+        int pick = 0;
+        if (coins[index] <= amount) {
+            pick = coinChangeUtil(index, amount - coins[index], coins, dp);
+        }
+        return dp[index][amount] = pick + notPick;
+    }
+public:
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+        return coinChangeUtil(n - 1, amount, coins, dp);
+    }
+};
